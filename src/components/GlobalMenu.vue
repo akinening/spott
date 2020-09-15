919848ce7f15e615f83f54d750aaa0f063fed8d4
小澤晶徳 <akinening@gmail.com>
2020-09-16 03:26:55 +0900

    router-link-exact-active でアクティブなタブが取れる

--- src/components/GlobalMenu.vue
+++ src/components/GlobalMenu.vue
@@ -4,8 +4,8 @@
             <img class="logo" src="https://akinening.github.io/bookmark/assets/logo.svg" alt="Spott">
         </a>
         <nav class="menu">
-            <a class="menu__list menu__list--active" href="./">TOP</a>
-            <a class="menu__list" href="./icon">アイコン</a>
+            <router-link to="/" class="menu__list">TOP</router-link>
+            <router-link to="/icon" class="menu__list">アイコン</router-link>
             <a class="menu__list" href="./illust">イラスト</a>
             <a class="menu__list" href="./color">配色</a>
             <a class="menu__list" href="./animation">アニメーション</a>
@@ -48,31 +48,31 @@ export default {
     padding 13px 0 13px 32px
     text-decoration none
 
-    &:hover:not(.menu__list--active)
+    &:hover:not(.router-link-exact-active)
       border-radius 100px
       background-color rgba(255, 255, 255, 0.25)
       transition background-color 0.25s
 
-    &--active
-      color #222
-      background-color #fff
-      border-radius 28px 0 0 28px
+  .router-link-exact-active
+    color #222
+    background-color #fff
+    border-radius 28px 0 0 28px
 
-      &::before,
-      &::after
-        content ''
-        position absolute
-        display block
-        width 28px
-        height 28px
-        right 0
-        background-image url('https://akinening.github.io/bookmark/assets/rounded.svg')
-        background-size cover
+    &::before,
+    &::after
+      content ''
+      position absolute
+      display block
+      width 28px
+      height 28px
+      right 0
+      background-image url('https://akinening.github.io/bookmark/assets/rounded.svg')
+      background-size cover
 
-      &::before
-        top -28px
+    &::before
+      top -28px
 
-      &::after
-        bottom -28px
-        transform scale(1, -1)
+    &::after
+      bottom -28px
+      transform scale(1, -1)
 </style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  data () {
+    return {
+      msg: 'アイコンのページです'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
