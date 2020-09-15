80f7b630b3099433f7fb404418019c9d58c86611
小澤晶徳 <akinening@gmail.com>
2020-09-16 03:51:27 +0900

    icon.vueを移行

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="styl">
 body {
   font-family:
     "Helvetica Neue",
--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    {{ msg }}
+    <a class="link-list" v-for="(link, num) in links" :href="link.url" :key="num" target="_blank">
+      {{ link.title }}
+    </a>
   </div>
 </template>
 
@@ -9,13 +11,42 @@ export default {
   name: 'Icon',
   data () {
     return {
-      msg: 'アイコンのページです'
+      links: [
+        {
+          url: 'https://glyphsearch.com/',
+          title: 'GlyphSearch - 複数サイトの一括検索'
+        },
+        {
+          url: 'https://material.io/resources/icons/?style=baseline',
+          title: 'Material Design'
+        },
+        {
+          url: 'https://fontawesome.com/cheatsheet',
+          title: 'Font Awesome'
+        },
+        {
+          url: 'https://icons.getbootstrap.com/',
+          title: 'Bootstrap Icon'
+        },
+        {
+          url: 'https://systemuicons.com/',
+          title: 'System UIcons'
+        },
+        {
+          url: 'https://icomoon.io/app/#/select',
+          title: 'IcoMoon Free'
+        },
+        {
+          url: 'https://icooon-mono.com/',
+          title: 'ICOOON MONO - フリーアイコン集'
+        }
+      ]
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="styl">
 @import "../assets/css/common.styl"
 
 </style>
