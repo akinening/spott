87b050d4e4384619f8919655ee8e4000c52d4945
小澤晶徳 <akinening@gmail.com>
2020-09-16 01:26:18 +0900

    stylusを導入してbemで書く

--- src/components/GlobalMenu.vue
+++ src/components/GlobalMenu.vue
@@ -4,12 +4,12 @@
             <img class="logo" src="https://akinening.github.io/bookmark/assets/logo.svg" alt="Spott">
         </a>
         <nav class="menu">
-            <a class="menu-list active" href="./">TOP</a>
-            <a class="menu-list" href="./icon">アイコン</a>
-            <a class="menu-list" href="./illust">イラスト</a>
-            <a class="menu-list" href="./color">配色</a>
-            <a class="menu-list" href="./animation">アニメーション</a>
-            <a class="menu-list" href="https://www.notion.so/akinen/Spott-79dc82356a2a4a15b52a696a9fbf8315">その他</a>
+            <a class="menu__list menu__list--active" href="./">TOP</a>
+            <a class="menu__list" href="./icon">アイコン</a>
+            <a class="menu__list" href="./illust">イラスト</a>
+            <a class="menu__list" href="./color">配色</a>
+            <a class="menu__list" href="./animation">アニメーション</a>
+            <a class="menu__list" href="https://www.notion.so/akinen/Spott-79dc82356a2a4a15b52a696a9fbf8315">その他</a>
         </nav>
     </aside>
 </template>
@@ -20,59 +20,58 @@ export default {
 }
 </script>
 
-<style scoped>
-.side {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 360px;
-    height: 100vh;
-    background-color: #111424;
-}
-.logo {
-    width: 160px;
-    margin: 32px 0 0 105px;
-}
-.menu {
-    display: flex;
-    flex-direction: column;
-    margin-top: 48px;
-    width: 255px;
-    float: right;
-}
-.menu-list {
-    position: relative;
-    color: #fff;
-    font-size: 20px;
-    font-weight: bold;
-    padding: 13px 0 13px 32px;
-}
-.menu-list:hover:not(.active) {
-    border-radius: 100px;
-    background-color: rgba(255, 255, 255, 0.25);
-    transition: background-color 0.25s;
-}
-.active {
-    color: #222;
-    background-color: #fff;
-    border-radius: 28px 0 0 28px;
-}
-.active::before,
-.active::after {
-    content: '';
-    position: absolute;
-    display: block;
-    width: 28px;
-    height: 28px;
-    right: 0;
-    background-image: url('https://akinening.github.io/bookmark/assets/rounded.svg');
-    background-size: cover;
-}
-.active::before {
-    top: -28px;
-}
-.active::after {
-    bottom: -28px;
-    transform: scale(1, -1);
-}
+<style lang="stylus" scoped>
+.side
+  position fixed
+  top 0
+  left 0
+  width 360px
+  height 100vh
+  background-color #111424
+
+.logo
+  width 160px
+  margin 32px 0 0 105px
+
+.menu
+  display flex
+  flex-direction column
+  margin-top 48px
+  width 255px
+  float right
+
+  &__list
+    position relative
+    color #fff
+    font-size 20px
+    font-weight bold
+    padding 13px 0 13px 32px
+
+    &:hover:not(.menu__list--active)
+      border-radius 100px
+      background-color rgba(255, 255, 255, 0.25)
+      transition background-color 0.25s
+
+    &--active
+      color #222
+      background-color #fff
+      border-radius 28px 0 0 28px
+
+      &::before,
+      &::after
+        content ''
+        position absolute
+        display block
+        width 28px
+        height 28px
+        right 0
+        background-image url('https://akinening.github.io/bookmark/assets/rounded.svg')
+        background-size cover
+
+      &::before
+        top -28px
+
+      &::after
+        bottom -28px
+        transform scale(1, -1)
 </style>
