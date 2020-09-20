39ad56f196e31613c32ea62405ba9b80dad0291e
小澤晶徳 <akinening@gmail.com>
2020-09-20 10:03:51 +0900

    TOPにリンクを追加

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -8,21 +8,56 @@
         <img class="banner__image" src="@/assets/image/location.svg" alt="map">
     </div>
 
-    <a class="card" href="https://dribbble.com/" target="_blank">
-        <img src="@/assets/image/dribbble.jpg" alt="dribbble">
-        <p>dribbble</p>
+    <a class="card" v-for="(card, num) in cards" :href="card.url" :key="num" target="_blank">
+        <img :src="card.img" :alt="card.title">
+        <p>{{ card.title }}</p>
     </a>
-    <a class="card" href="https://www.pinterest.jp/" target="_blank">
-        <img src="@/assets/image/pinterest.jpg"
-            alt="pinterest">
-        <p>Pinterest</p>
+
+    <a class="link" v-for="(link, num) in links" :href="link.url" :key="num" target="_blank">
+      <p class="link__title">{{ link.title }}</p>
+      <p class="link__url">{{ link.url }}</p>
     </a>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Index'
+  name: 'Index',
+  data () {
+    return {
+      cards: [
+        {
+          url: '"https://dribbble.com/',
+          img: require('@/assets/image/dribbble.jpg'),
+          title: 'dribbble'
+        },
+        {
+          url: 'https://www.pinterest.jp/',
+          img: require('@/assets/image/pinterest.jpg'),
+          title: 'Pinterest'
+        }
+      ],
+      links: [
+        {
+          url: 'https://www.evernote.design/',
+          title: 'All in One Bookmark Links'
+        },
+        {
+          url: 'https://www.designresourc.es/',
+          title: 'いいね順のDesign Resources'
+        },
+        {
+          url: 'https://www.designnotes.co/',
+          title: 'Design Resource一覧'
+        },
+        {
+          url: 'https://www.uplabs.com/',
+          title: 'uplabs - デザインファイル共有サイト'
+        }
+      ]
+    }
+  }
 }
 </script>
 
